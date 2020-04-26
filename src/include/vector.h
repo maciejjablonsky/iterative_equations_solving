ad4758a2fe7cf072b4547444db6644d9d0479bc9
maciejjablonsky <maciekjablonsky@gmail.com>
2020-04-26 16:50:37 +0200

    Added comments for vector member functions

--- src/include/vector.h
+++ src/include/vector.h
@@ -6,24 +6,40 @@
 #include <string.h>
 #include <math.h>
 
+/** @file */
+
 
 typedef unsigned int len_t;
 typedef unsigned int uint;
-typedef double element_t;
+typedef double element_t; /**< Type of element stored in vectors or matrices */
 
+/**
+ * @brief Struct to store data of vector
+ */
 struct vector {
-    element_t * elements;
-    len_t len;
+    element_t * elements; /**< Continuous  block of memory containing elements of vector */
+    len_t len; /**< Length of vector */
 };
 
-
+/**
+ * @brief Macro simplifying initialization of \ref vector
+ */
 #define vector_struct(...) (struct vector){__VA_ARGS__}
 
+/**
+ * @brief Computes scalar product of two equal length vectors
+ * @param vecA Elements of left vector
+ * @param vecB Elements of right vector
+ * @param len Length of each vector
+ * @return Result of computing as scalar value
+ */
 element_t vector__dot_product(const element_t *vecA, const element_t *vecB, int len);
 
-element_t *
-vectors__dot_products(element_t *vectorsA, element_t *vectorsB, size_t number_of_vectors, size_t each_vector_length);
-
+/**
+ * @brief Computes euclidean norm of \ref vector (length of vector)
+ * @param vec Input vector
+ * @return Result norm
+ */
 element_t vector__norm(struct vector * vec);
 
 
--- src/vector.c
+++ src/vector.c
@@ -11,20 +11,6 @@ element_t vector__dot_product(const element_t *vecA, const element_t *vecB, int 
 	return dot_product;
 }
 
-element_t *
-vectors__dot_products(element_t *vectorsA, element_t *vectorsB, size_t number_of_vectors, size_t each_vector_length) {
-	element_t *result = malloc(number_of_vectors * sizeof(*result));
-	if (!result) {
-		return NULL;
-	}
-
-	for (int i = 0; i < number_of_vectors; ++i) {
-		size_t offset = i * each_vector_length;
-		result[i] = vector__dot_product(vectorsA + offset, vectorsB + offset, each_vector_length);
-	}
-	return result;
-}
-
 static inline long double square(element_t val) {
         return val * val;
 }
