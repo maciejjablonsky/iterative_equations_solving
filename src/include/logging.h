9f8e55fb0abce099d3ea1f86c7f776143e363cdc
maciejjablonsky <maciekjablonsky@gmail.com>
2020-04-26 16:35:15 +0200

    Added comments for LOG macros

--- src/include/logging.h
+++ src/include/logging.h
@@ -2,20 +2,30 @@
 #define LOG_H
 
 #include <stdio.h>
-//
+
+/** @file */
+
 //#define DEBUG
 
 #ifdef DEBUG
+/**
+ * @brief Macro used to log error messages to stderr with printf's like format
+ */
 #define LOG_ERROR(...) do {\
 fprintf(stderr, "[FILE: %s][LINE: %d] ", __FILE__, __LINE__);\
 fprintf(stderr, __VA_ARGS__);\
 fputc('\n', stderr);\
 } while(0)
 #else
+/**
+* @brief Macro used to log error messages to stderr with printf's like format
+*/
 #define LOG_ERROR(...)
 #endif
 
-
+/**
+ * @brief Macro used to log messages to stdout with printf's like format
+ */
 #define LOG(...) do{\
 fprintf(stdout, "[FILE: %s][LINE: %d] ", __FILE__, __LINE__);\
 fprintf(stdout, __VA_ARGS__);\
